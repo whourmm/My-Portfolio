429225ef4b062838ff9efe50ca3418adb7fa104e
NaMely YuMMy <andavarinthorn1409@gmail.com>
2024-01-17 02:56:36 +0700

    setup website

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-black">
+        <div className="bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
