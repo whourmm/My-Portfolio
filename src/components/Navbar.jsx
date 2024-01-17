64c899eb8a4dbdf2270c1153c2d0277bf4f2794b
NaMely YuMMy <andavarinthorn1409@gmail.com>
2024-01-18 00:14:08 +0700

    implement Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,9 @@ import { Link } from "react-router-dom";
 import { styles } from "../styles";
 import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
+import { motion } from "framer-motion";
+import { fadeInAnimationVariants } from "../utils/motion";
+import { fadeInAnimationVariantsTab } from "../utils/motion";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
@@ -22,8 +25,8 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer ">
-            Anda <span className="sm:block hidden">| Varinthorn</span>
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Anda &nbsp;<span className="md:block hidden">| Varinthorn</span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex flex-row gap-10">
@@ -37,15 +40,37 @@ const Navbar = () => {
                 setActive(link.title);
               }}
             >
-              <a href={`${link.id}`}>{link.title}</a>
+              <a href={`#${link.id}`}>{link.title}</a>
             </li>
           ))}
         </ul>
-        <div className="sm:hidden flex flex-1 justify-end items-center">
+
+        <div className=" sm:hidden flex flex-1 justify-end items-center">
+          <motion.div
+            className={` ${
+              toggle
+                ? "fixed absolute w-[200px] mr-[-50px] opacity-10 z-10 black-gradient h-screen bg-white mt-[90.5vh] "
+                : "hidden"
+            }`}
+            transition={{ type: "spring", bounce: 0.25 }}
+            variants={fadeInAnimationVariantsTab}
+            initial="initial"
+            whileInView={toggle ? "animate" : "none"}
+            viewport={{
+              once: true,
+            }}
+          />
+          <div
+            className={`${
+              toggle
+                ? "absolute w-screen h-screen bg-black mt-[90.5vh] z-0 opacity-50"
+                : "hidden"
+            } `}
+          />
           <img
             src={toggle ? close : menu}
             alt="menu"
-            className="w-[28px] h=[28px] object-contain cursor-pointer"
+            className="z-50 w-[28px] h=[28px] object-contain cursor-pointer"
             onClick={() => {
               setToggle(!toggle);
             }}
@@ -53,23 +78,29 @@ const Navbar = () => {
           <div
             className={`${
               !toggle ? "hidden" : "flex"
-            } p-6 absolute top-20 rounded-xl min-w-[140px] my-2 mx-4 z-10 black-gradient`}
+            } p-6 absolute top-20 rounded-xl min-w-[100px] z-10  flex flex-col gap-5`}
           >
-            <ul className="list-none flex flex-col gap-10 justify-end items-start gap-4 ">
-              {navLinks.map((link) => (
-                <li
-                  key={link.id}
-                  className={`${
-                    active === link.title ? "text-white" : "text-secondary"
-                  } hover:text-white font-medium cursor-pointer`}
-                  onClick={() => {
-                    setActive(link.title);
-                  }}
-                >
-                  <a href={`${link.id}`}>{link.title}</a>
-                </li>
-              ))}
-            </ul>
+            {navLinks.map((link, i) => (
+              <motion.ol
+                className={`${
+                  active === link.title ? "text-white" : "text-secondary"
+                } hover:text-white font-medium cursor-pointer`}
+                onClick={() => {
+                  setActive(link.title);
+                  setToggle(!toggle);
+                }}
+                transition={{ type: "spring", bounce: 0.25 }}
+                variants={fadeInAnimationVariants}
+                initial="initial"
+                whileInView={toggle ? "animate" : "none"}
+                viewport={{
+                  once: true,
+                }}
+                custom={i}
+              >
+                <a href={`#${link.id}`}>{link.title}</a>
+              </motion.ol>
+            ))}
           </div>
         </div>
       </div>
--- src/utils/motion.js
+++ src/utils/motion.js
@@ -15,74 +15,191 @@ export const textVariant = (delay) => {
       },
     };
   };
-  
-  export const fadeIn = (direction, type, delay, duration) => {
-    return {
-      hidden: {
-        x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
-        y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
-        opacity: 0,
+
+
+
+  export const navVariants = {
+    hidden: {
+      opacity: 0,
+      y: -50,
+      transition: {
+        type: 'spring',
+        stiffness: 300,
+        damping: 140,
       },
-      show: {
-        x: 0,
-        y: 0,
-        opacity: 1,
-        transition: {
-          type: type,
-          delay: delay,
-          duration: duration,
-          ease: "easeOut",
-        },
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: 'spring',
+        stiffness: 80,
+        delay: 1,
       },
-    };
+    },
   };
   
-  export const zoomIn = (delay, duration) => {
-    return {
-      hidden: {
-        scale: 0,
-        opacity: 0,
+  export const slideIn = (direction, type, delay, duration) => ({
+    hidden: {
+      x: direction === 'left' ? '-100%' : direction === 'right' ? '100%' : 0,
+      y: direction === 'up' ? '100%' : direction === 'down' ? '100%' : 0,
+    },
+    show: {
+      x: 0,
+      y: 0,
+      transition: {
+        type,
+        delay,
+        duration,
+        ease: 'easeOut',
       },
-      show: {
-        scale: 1,
-        opacity: 1,
-        transition: {
-          type: "tween",
-          delay: delay,
-          duration: duration,
-          ease: "easeOut",
-        },
+    },
+  });
+  
+  export const staggerContainer = (staggerChildren, delayChildren) => ({
+    hidden: {},
+    show: {
+      transition: {
+        staggerChildren,
+        delayChildren,
       },
-    };
+    },
+  });
+  
+
+  export const textContainer = {
+    hidden: {
+      opacity: 0,
+    },
+    show: (i = 1) => ({
+      opacity: 1,
+      transition: { staggerChildren: 0.1, delayChildren: i * 0.1 },
+    }),
   };
   
-  export const slideIn = (direction, type, delay, duration) => {
-    return {
-      hidden: {
-        x: direction === "left" ? "-100%" : direction === "right" ? "100%" : 0,
-        y: direction === "up" ? "100%" : direction === "down" ? "100%" : 0,
+  export const textVariant2 = {
+    hidden: {
+      opacity: 0,
+      y: 20,
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: 'tween',
+        ease: 'easeIn',
       },
-      show: {
-        x: 0,
-        y: 0,
-        transition: {
-          type: type,
-          delay: delay,
-          duration: duration,
-          ease: "easeOut",
-        },
-      },
-    };
+    },
   };
   
-  export const staggerContainer = (staggerChildren, delayChildren) => {
-    return {
-      hidden: {},
-      show: {
-        transition: {
-          staggerChildren: staggerChildren,
-          delayChildren: delayChildren || 0,
-        },
+  export const fadeIn = (direction, type, delay, duration) => ({
+    hidden: {
+      x: direction === 'left' ? 100 : direction === 'right' ? -100 : 0,
+      y: direction === 'up' ? 100 : direction === 'down' ? -100 : 0,
+      opacity: 0,
+    },
+    show: {
+      x: 0,
+      y: 0,
+      opacity: 1,
+      transition: {
+        type,
+        delay,
+        duration,
+        ease: 'easeOut',
       },
-    };
+    },
+  });
+  
+  export const planetVariants = (direction) => ({
+    hidden: {
+      x: direction === 'left' ? '-100%' : '100%',
+      rotate: 120,
+    },
+    show: {
+      x: 0,
+      rotate: 0,
+      transition: {
+        type: 'spring',
+        duration: 1.8,
+        delay: 0.5,
+      },
+    },
+  });
+  
+  export const zoomIn = (delay, duration) => ({
+    hidden: {
+      scale: 0,
+      opacity: 0,
+    },
+    show: {
+      scale: 1,
+      opacity: 1,
+      transition: {
+        type: 'tween',
+        delay,
+        duration,
+        ease: 'easeOut',
+      },
+    },
+  });
+  
+  export const footerVariants = {
+    hidden: {
+      opacity: 0,
+      y: 50,
+      transition: {
+        type: 'spring',
+        stiffness: 300,
+        damping: 140,
+      },
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: 'spring',
+        stiffness: 80,
+        delay: 0.5,
+      },
+    },
+  };
+
+  export const fadeInAnimationVariants = {
+    initial: {
+      opacity: 0,
+      x: -30,
+    },
+    animate: (i) => ({
+      opacity: 1,
+      x: 0,
+      transition: {
+        delay: (0.2 * i) + 0.2,
+      },
+    }),
+  };
+  export const fadeInAnimationVariantsTab = {
+    initial: {
+      opacity: 0,
+      x: 60,
+    },
+    animate: (i) => ({
+      opacity: 1,
+      x: 0,
+      transition: {
+        delay: 0,
+      },
+    }),
+  };
+
+  export const backgroundFade = {
+    initial: {
+      opacity: 0,
+    },
+    animate: () => ({
+      opacity: 1,
+      transition: {
+        duration: 2,
+      },
+    }),
   };
